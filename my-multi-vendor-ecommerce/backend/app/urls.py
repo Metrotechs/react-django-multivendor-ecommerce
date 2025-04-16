c91119a1b899c1e5e521aa2686c6e09ee91d5b8c
Metrotechs <richard@metrotechs.net>
2025-04-16 11:23:25 -0500

    Delete my-multi-vendor-ecommerce directory

--- my-multi-vendor-ecommerce/backend/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProductListView, VendorListView, OrderListView
-
-urlpatterns = [
-    path('products/', ProductListView.as_view(), name='product-list'),
-    path('vendors/', VendorListView.as_view(), name='vendor-list'),
-    path('orders/', OrderListView.as_view(), name='order-list'),
-]
